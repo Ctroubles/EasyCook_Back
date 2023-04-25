3b4b9c2c852c2072081201d986445d94963b9458
troubles <cesar.troubles@gmail.com>
2023-04-25 09:51:51 -0500

    debuggin attempt

--- src/db.js
+++ src/db.js
@@ -5,9 +5,11 @@ const {
   DB_NAME, DB_PASSWORD,
 } = process.env;
 
-main().catch(err => console.log(err));
+main()
+.then(value=>console.log("all good"))
+.catch(err => console.log(err));
 
 async function main() {
   mongoose.set('strictQuery', true);
-  await mongoose.connect(`mongodb+srv://admin:4AwnTIg0ALbFF1hi@cluster0.reehusu.mongodb.net/EasyCook?retryWrites=true&w=majority`, { useNewUrlParser: true });
+  await mongoose.connect(`mongodb+srv://admin:${DB_PASSWORD}@cluster0.pku4cwo.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`, { useNewUrlParser: true });
 }
