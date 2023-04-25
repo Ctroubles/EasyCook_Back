9bb6ad5356e7a0b24bd7332b582565a992ae190a
troubles <cesar.troubles@gmail.com>
2023-04-25 08:29:40 -0500

    debuggin attempt

--- src/db.js
+++ src/db.js
@@ -1,13 +1,13 @@
-// require('dotenv').config();
-// const mongoose = require("mongoose");
+require('dotenv').config();
+const mongoose = require("mongoose");
 
-// const {
-//   DB_NAME, DB_PASSWORD,
-// } = process.env;
+const {
+  DB_NAME, DB_PASSWORD,
+} = process.env;
 
-// main().catch(err => console.log(err));
+main().catch(err => console.log(err));
 
-// async function main() {
-//   mongoose.set('strictQuery', true);
-//   await mongoose.connect(`mongodb+srv://admin:${DB_PASSWORD}@cluster0.pku4cwo.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`, { useNewUrlParser: true });
-// }
+async function main() {
+  mongoose.set('strictQuery', true);
+  await mongoose.connect(`mongodb+srv://admin:${DB_PASSWORD}@cluster0.pku4cwo.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`, { useNewUrlParser: true });
+}
