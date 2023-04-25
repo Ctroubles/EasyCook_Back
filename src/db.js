f30e37ace5f07938173bae510d2d1faa3752c7fd
troubles <cesar.troubles@gmail.com>
2023-04-25 09:01:21 -0500

    debuggin attempt

--- src/db.js
+++ src/db.js
@@ -5,10 +5,9 @@ const {
   DB_NAME, DB_PASSWORD,
 } = process.env;
 
-async function main() {
-  mongoose.set('strictQuery', true);
-  await mongoose.connect(`mongodb+srv://admin:4AwnTIg0ALbFF1hi@cluster0.pku4cwo.mongodb.net/EasyCook?retryWrites=true&w=majority`, { useNewUrlParser: true });
-}
-
 main().catch(err => console.log(err));
 
+async function main() {
+  mongoose.set('strictQuery', true);
+  await mongoose.connect(`mongodb+srv://admin:4AwnTIg0ALbFF1hi@cluster0.reehusu.mongodb.net/EasyCook?retryWrites=true&w=majority`, { useNewUrlParser: true });
+}
