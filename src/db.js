2e1d4a7cb80368f89f2d7686451d0f9f5fc953b6
troubles <cesar.troubles@gmail.com>
2023-04-25 10:04:10 -0500

    debuggin attempt

--- src/db.js
+++ src/db.js
@@ -12,5 +12,5 @@ main()
 async function main() {
   mongoose.set('strictQuery', true);
   // await mongoose.connect(`mongodb+srv://admin:${DB_PASSWORD}@cluster0.pku4cwo.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`, { useNewUrlParser: true });
-  await mongoose.connect(`mongodb+srv://admin:${DB_PASSWORD}@cluster0.pku4cwo.mongodb.net/?retryWrites=true&w=majority`, { useNewUrlParser: true });
+  await mongoose.connect(`mongodb+srv://admin:PICKQaC4Dhfo2Xo0@cluster0.pku4cwo.mongodb.net/?retryWrites=true&w=majority`, { useNewUrlParser: true });
 }
