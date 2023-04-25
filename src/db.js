31dcb8023dbb7866dbbb9cc479c4e3edca1fdec2
troubles <cesar.troubles@gmail.com>
2023-04-25 10:42:55 -0500

    debuggin attempt

--- src/db.js
+++ src/db.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const mongoose = require("mongoose");
-const fs = require("fs")
+
+
 const {
   DB_NAME, DB_PASSWORD,
 } = process.env;
@@ -8,17 +9,11 @@ const {
 main()
 .then(value=>console.log("se conectó correctamente"))
 .catch(err => {
-  fs.appendFile("docu.txt", err.toString(), error=>{
-    if (error) {
-      console.log(error);
-    }
-    console.log("se escribió todo bien creo");
-  });
   console.log(err);
 })
 
 async function main() {
   mongoose.set('strictQuery', true);
   // await mongoose.connect(`mongodb+srv://admin:${DB_PASSWORD}@cluster0.pku4cwo.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`, { useNewUrlParser: true });
-  await mongoose.connect(`mongodb+srv://cesaruser:BeEONKiiBh6HBDMw@clusteraws0.j7zjhor.mongodb.net/?retryWrites=true&w=majority`, { useNewUrlParser: true });
+  await mongoose.connect(`mongodb+srv://tex_user:8SZ8tlEcnjVI16TR@cluster0aws.reehusu.mongodb.net/tex-db?retryWrites=true&w=majority`, { useNewUrlParser: true });
 }
