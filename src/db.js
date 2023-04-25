68b9fbaaf4f7b98a64d56f6ba46d2690e204b370
troubles <cesar.troubles@gmail.com>
2023-04-25 08:33:19 -0500

    debuggin attempt

--- src/db.js
+++ src/db.js
@@ -1,13 +1,13 @@
-require('dotenv').config();
-const mongoose = require("mongoose");
+// require('dotenv').config();
+// const mongoose = require("mongoose");
 
 // const {
 //   DB_NAME, DB_PASSWORD,
 // } = process.env;
 
-main().catch(err => console.log(err));
+// main().catch(err => console.log(err));
 
-async function main() {
-  mongoose.set('strictQuery', true);
-  await mongoose.connect(`mongodb+srv://admin:4AwnTIg0ALbFF1hi@cluster0.pku4cwo.mongodb.net/EasyCook?retryWrites=true&w=majority`, { useNewUrlParser: true });
-}
+// async function main() {
+//   mongoose.set('strictQuery', true);
+//   await mongoose.connect(`mongodb+srv://admin:4AwnTIg0ALbFF1hi@cluster0.pku4cwo.mongodb.net/EasyCook?retryWrites=true&w=majority`, { useNewUrlParser: true });
+// }
