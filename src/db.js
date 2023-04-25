ee820477cfb6ebc0d0f3f3266f0ff931a6adc3b7
troubles <cesar.troubles@gmail.com>
2023-04-25 10:24:07 -0500

    debuggin attempt

--- src/db.js
+++ src/db.js
@@ -1,16 +1,25 @@
 require('dotenv').config();
 const mongoose = require("mongoose");
-
+const fs = require("fs")
 const {
   DB_NAME, DB_PASSWORD,
 } = process.env;
 
 main()
-.then(value=>console.log("all good"))
-.catch(err => console.log(err));
+.then(value=>console.log("se conectó correctamente"))
+.catch(err => {
+  fs.appendFile("docu.txt", err.toString(), error=>{
+    if (error) {
+      console.log(error);
+    }
+    console.log("se escribió todo bien creo");
+  });
 
+})
+
+console.log("hey");
 async function main() {
   mongoose.set('strictQuery', true);
   // await mongoose.connect(`mongodb+srv://admin:${DB_PASSWORD}@cluster0.pku4cwo.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`, { useNewUrlParser: true });
-  await mongoose.connect(`mongodb+srv://admin:PICKQaC4Dhfo2Xo0@cluster0.pku4cwo.mongodb.net/?retryWrites=true&w=majority`, { useNewUrlParser: true });
+  await mongoose.connect(`mongodb+srv://cesaruser:BeEONKiiBh6HBDMw@clusteraws0.j7zjhor.mongodb.net/?retryWrites=true&w=majority`, { useNewUrlParser: true });
 }
